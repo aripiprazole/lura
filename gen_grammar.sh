8cb6f2854f1562fa4a8eafdb2561ac714a32fa5a
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2023-07-18 11:50:16 +0000

    feat(lura-syntax): add generated code

---
+++ gen_grammar.sh
@@ -0,0 +1,5 @@
+type-sitter-cli vendor/tree-sitter-lura/src/node-types.json \
+  -t node-types \
+  --output-dir lura-syntax/src/generated \
+  --language tree-sitter-lura \
+  --use-wrapper
---
+++ lura-syntax/src/lib.rs
@@ -0,0 +1,13 @@
+#[allow(clippy::all)]
+#[allow(unused_variables)]
+#[allow(dead_code)]
+#[allow(non_snake_case)]
+#[allow(unused_macros)]
+#[allow(unused_parens)]
+#[allow(unused_labels)]
+#[allow(non_upper_case_globals)]
+pub mod generated {
+    pub mod lura;
+}
+
+pub use generated::lura::*;
