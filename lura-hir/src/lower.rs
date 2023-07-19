848bad3d86eb7197c03738097290e0fa6fae5d76
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2023-07-19 14:42:32 +0000

    feat(lura-hir): add hir

--- lura-hir/src/lower.rs
+++ lura-hir/src/lower.rs
@@ -1,10 +1,74 @@
-use lura_syntax::Source;
+use std::sync::Arc;
 
-use crate::source::{HirSource, HirSourceId};
+use if_chain::if_chain;
+use lura_syntax::{generated::lura::SourceFile, Source};
+use tree_sitter::{Node, Tree};
+use type_sitter_lib::ExtraOr;
+
+use crate::source::{top_level::TopLevel, HirSource, HirSourceId};
 
 #[salsa::tracked]
 pub fn hir_lower(db: &dyn crate::HirDb, source: Source) -> HirSource {
     let id = HirSourceId::new(db, source.file_path(db).clone());
+    let parse_tree = source.syntax_node(db);
 
-    HirSource::new(db, id, source, vec![])
+    let lower = LowerHir {
+        db,
+        source,
+        tree: parse_tree.tree.clone(),
+        root_node: parse_tree.tree.root_node(),
+        source_id: id,
+    };
+
+    lower.hir_source()
+}
+
+struct LowerHir<'db, 'tree> {
+    db: &'db dyn crate::HirDb,
+    source: Source,
+    tree: Arc<Tree>,
+    root_node: Node<'tree>,
+    source_id: HirSourceId,
+}
+
+#[rustfmt::skip]
+mod concrete {
+    pub type Decl<'tree> = lura_syntax::anon_unions::ClassDecl_Clause_Command_DataDecl_Signature_TraitDecl_Using<'tree>;
+}
+
+impl<'db, 'tree> LowerHir<'db, 'tree> {
+    pub fn hir_source(&self) -> HirSource {
+        let ast = SourceFile::try_from(self.root_node).unwrap();
+
+        let decls: Vec<_> = ast
+            .decls(&mut self.tree.walk())
+            .map(|node| {
+                if_chain! {
+                    if let Ok(node) = node;
+                    if let ExtraOr::Regular(node) = node;
+                    then {
+                        // Process declaration only if it is not an error, or it's not a junk
+                        // declaration.
+                        return self.hir_decl(node);
+                    }
+                }
+
+                TopLevel::Error
+            })
+            .collect();
+
+        HirSource::new(self.db, self.source_id, self.source, decls)
+    }
+
+    pub fn hir_decl(&self, decl: concrete::Decl) -> TopLevel {
+        match decl {
+            concrete::Decl::ClassDecl(_) => todo!(),
+            concrete::Decl::Clause(_) => todo!(),
+            concrete::Decl::Command(_) => todo!(),
+            concrete::Decl::DataDecl(_) => todo!(),
+            concrete::Decl::Signature(_) => todo!(),
+            concrete::Decl::TraitDecl(_) => todo!(),
+            concrete::Decl::Using(_) => todo!(),
+        }
+    }
 }
--- lura-hir/src/source.rs
+++ lura-hir/src/source.rs
@@ -1,6 +1,18 @@
-use std::path::PathBuf;
+#![allow(clippy::too_many_arguments)]
+
+use std::{collections::HashSet, path::PathBuf};
 
 use lura_syntax::Source;
+
+pub trait HirElement {
+    /// The range of the element in the source file.
+    fn location(&self) -> TextRange;
+}
+
+#[salsa::tracked]
+pub struct HirError {
+    pub location: TextRange,
+}
 
 #[salsa::tracked]
 pub struct HirSource {
@@ -9,7 +21,7 @@ pub struct HirSource {
     pub source: Source,
 
     #[return_ref]
-    pub contents: Vec<TopLevel>,
+    pub contents: Vec<top_level::TopLevel>,
 }
 
 #[salsa::interned]
@@ -18,5 +30,251 @@ pub struct HirSourceId {
     pub path: PathBuf,
 }
 
+#[salsa::tracked]
+pub struct QualifiedPath {}
+
+#[salsa::tracked]
+pub struct Identifier {}
+
 #[derive(Clone, Hash, PartialEq, Eq, Debug)]
-pub enum TopLevel {}
+pub struct TextRange {}
+
+#[derive(Clone, Hash, PartialEq, Eq, Debug)]
+pub struct Spanned<T> {
+    pub value: T,
+    pub location: TextRange,
+}
+
+pub mod declaration {
+    use super::*;
+
+    pub trait Declaration: HirElement {
+        fn attributes(&self) -> HashSet<Attribute>;
+        fn visibility(&self) -> Visibility;
+        fn docs(&self) -> Vec<DocString>;
+        fn name(&self) -> QualifiedPath;
+        fn parameters(&self) -> Vec<Parameter>;
+        fn type_rep(&self) -> Option<type_rep::TypeRep>;
+    }
+
+    #[salsa::tracked]
+    pub struct Attribute {
+        pub name: QualifiedPath,
+        pub arguments: Vec<expr::Expr>,
+        pub location: TextRange,
+    }
+
+    #[salsa::tracked]
+    pub struct DocString {
+        pub range: TextRange,
+    }
+
+    #[derive(Default, Clone, Hash, PartialEq, Eq, Debug)]
+    pub enum Visibility {
+        #[default]
+        Public,
+        Sealed,
+        Private,
+        Internal,
+    }
+
+    #[salsa::tracked]
+    pub struct Parameter {
+        pub binding: pattern::Pattern,
+        pub parameter_type: type_rep::TypeRep,
+
+        /// Whether this parameter is implicit, i.e. it's a `forall` parameter.
+        pub is_implicit: bool,
+        pub location: TextRange,
+    }
+}
+
+pub mod top_level {
+    use super::*;
+
+    #[salsa::tracked]
+    pub struct Signature {
+        pub attributes: HashSet<declaration::Attribute>,
+        pub visibility: Spanned<declaration::Visibility>,
+        pub docs: Vec<declaration::DocString>,
+        pub name: QualifiedPath,
+        pub parameters: Vec<declaration::Parameter>,
+        pub return_type: type_rep::TypeRep,
+        pub body: stmt::Block,
+        pub location: TextRange,
+    }
+
+    #[salsa::tracked]
+    pub struct Clause {
+        pub name: QualifiedPath,
+        pub arguments: Vec<pattern::Pattern>,
+        pub value: expr::Expr,
+        pub location: TextRange,
+    }
+
+    #[derive(Clone, Hash, PartialEq, Eq, Debug)]
+    pub enum TopLevel {
+        Error,
+        Clause(Clause),
+        Signature(Signature),
+    }
+}
+
+pub mod pattern {
+    use super::*;
+
+    #[derive(Clone, Hash, PartialEq, Eq, Debug)]
+    pub enum Constructor {
+        Array,
+        Tuple,
+        Unit,
+        Path(QualifiedPath),
+    }
+
+    #[salsa::tracked]
+    pub struct ConstructorPattern {
+        pub name: Constructor,
+        pub arguments: Vec<pattern::Pattern>,
+        pub location: TextRange,
+    }
+
+    #[salsa::tracked]
+    pub struct BindingPattern {
+        pub name: Identifier,
+        pub location: TextRange,
+    }
+
+    #[derive(Clone, Hash, PartialEq, Eq, Debug)]
+    pub enum Pattern {
+        Error,
+        Constructor(ConstructorPattern),
+        Binding(BindingPattern),
+    }
+}
+
+pub mod stmt {
+    use super::*;
+
+    #[salsa::tracked]
+    pub struct AskStmt {
+        pub pattern: pattern::Pattern,
+        pub value: expr::Expr,
+        pub location: TextRange,
+    }
+
+    #[salsa::tracked]
+    pub struct LetStmt {
+        pub pattern: pattern::Pattern,
+        pub value: expr::Expr,
+        pub location: TextRange,
+    }
+
+    #[derive(Clone, Hash, PartialEq, Eq, Debug)]
+    pub enum Stmt {
+        Error(HirError),
+        Ask(AskStmt),
+        Let(LetStmt),
+        Expr(expr::Expr),
+    }
+
+    #[salsa::tracked]
+    pub struct Block {
+        pub statements: Vec<Stmt>,
+        pub return_value: Option<expr::Expr>,
+        pub location: TextRange,
+    }
+}
+
+pub mod expr {
+    use super::*;
+
+    #[derive(Clone, Hash, PartialEq, Eq, Debug)]
+    pub enum MatchKind {
+        If,
+        Match,
+        StmtLevel(Box<MatchKind>),
+    }
+
+    #[derive(Clone, Hash, PartialEq, Eq, Debug)]
+    pub enum Callee {
+        Array,
+        Tuple,
+        Unit,
+        Expr(Box<expr::Expr>),
+    }
+
+    #[derive(Clone, Hash, PartialEq, Eq, Debug)]
+    pub enum CallKind {
+        Error,
+        Infix,
+        Prefix,
+    }
+
+    #[salsa::tracked]
+    pub struct AbsExpr {
+        pub parameters: Vec<declaration::Parameter>,
+        pub value: Box<expr::Expr>,
+        pub location: TextRange,
+    }
+
+    #[salsa::tracked]
+    pub struct AnnExpr {
+        pub value: Box<expr::Expr>,
+        pub type_rep: type_rep::TypeRep,
+        pub location: TextRange,
+    }
+
+    #[derive(Clone, Hash, PartialEq, Eq, Debug)]
+    pub struct MatchArm {
+        pub pattern: pattern::Pattern,
+        pub value: expr::Expr,
+        pub location: TextRange,
+    }
+
+    #[salsa::tracked]
+    pub struct MatchExpr {
+        pub kind: MatchKind,
+        pub scrutinee: Box<expr::Expr>,
+        pub clauses: Vec<MatchArm>,
+    }
+
+    #[salsa::tracked]
+    pub struct CallExpr {
+        pub kind: CallKind,
+        pub callee: Callee,
+        pub arguments: Vec<expr::Expr>,
+        pub do_notation: stmt::Block,
+        pub location: TextRange,
+    }
+
+    #[derive(Clone, Hash, PartialEq, Eq, Debug)]
+    pub enum Expr {
+        Error(HirError),
+        Path(QualifiedPath),
+        Call(CallExpr),
+        Ann(AnnExpr),
+        Abs(AbsExpr),
+        Match(MatchExpr),
+        Upgrade(Box<expr::type_rep::TypeRep>),
+    }
+}
+
+pub mod type_rep {
+    use super::*;
+
+    #[salsa::tracked]
+    pub struct QPath {
+        /// Usually a trait type path with associated type bindings, like `Foo.Bar.Baz`.
+        pub qualifier: QualifiedPath,
+        pub name: Identifier,
+        pub location: TextRange,
+    }
+
+    #[derive(Clone, Hash, PartialEq, Eq, Debug)]
+    pub enum TypeRep {
+        Error(HirError),
+        Path(QualifiedPath),
+        QPath(QPath),
+        Downgrade(Box<expr::Expr>),
+    }
+}
