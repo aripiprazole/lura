2ec8e3925bd07aec1cd4ff55854f8cdb1a3a8997
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2023-07-29 21:44:55 +0000

    feat(lura-typer): add base for typer

---
+++ lura-typer/src/lib.rs
@@ -0,0 +1,17 @@
+use lura_hir::HirDb;
+use salsa::DbWithJar;
+
+extern crate salsa_2022 as salsa;
+
+#[salsa::jar(db = TyperDb)]
+pub struct Jar;
+
+/// The database that Typer uses internally. This is a trait so that we can
+/// mock it during testing.
+pub trait TyperDb: DbWithJar<Jar> {}
+
+impl<DB> TyperDb for DB where DB: ?Sized + HirDb + salsa::DbWithJar<Jar> {}
+
+pub mod coverage;
+pub mod infer;
+pub mod thir;
