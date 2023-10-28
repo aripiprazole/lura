7e13236cc38aa62f08e7287261324fe35f979d57
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2023-10-28 18:00:21 -0300

    style(tree-sitter-lura): improve codestyle

--- tree-sitter-lura/bindings/rust/lib.rs
+++ tree-sitter-lura/bindings/rust/lib.rs
@@ -6,7 +6,9 @@
 //! ```
 //! let code = "";
 //! let mut parser = tree_sitter::Parser::new();
-//! parser.set_language(tree_sitter_asena::language()).expect("Error loading asena grammar");
+//! parser
+//!   .set_language(tree_sitter_asena::language())
+//!   .expect("Error loading asena grammar");
 //! let tree = parser.parse(code, None).unwrap();
 //! ```
 //!
@@ -18,14 +20,14 @@
 use tree_sitter::Language;
 
 extern "C" {
-    fn tree_sitter_lura() -> Language;
+  fn tree_sitter_lura() -> Language;
 }
 
 /// Get the tree-sitter [Language][] for this grammar.
 ///
 /// [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
 pub fn language() -> Language {
-    unsafe { tree_sitter_lura() }
+  unsafe { tree_sitter_lura() }
 }
 
 /// The content of the [`node-types.json`][] file for this grammar.
@@ -36,17 +38,17 @@ pub const NODE_TYPES: &'static str = include_str!("../../src/node-types.json");
 // Uncomment these to include any queries that this grammar contains
 
 pub const HIGHLIGHTS_QUERY: &'static str = include_str!("../../queries/highlights.scm");
-// pub const INJECTIONS_QUERY: &'static str = include_str!("../../queries/injections.scm");
 pub const LOCALS_QUERY: &'static str = include_str!("../../queries/locals.scm");
+// pub const INJECTIONS_QUERY: &'static str = include_str!("../../queries/injections.scm");
 // pub const TAGS_QUERY: &'static str = include_str!("../../queries/tags.scm");
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn test_can_load_grammar() {
-        let mut parser = tree_sitter::Parser::new();
-        parser
-            .set_language(super::language())
-            .expect("Error loading asena language");
-    }
+  #[test]
+  fn test_can_load_grammar() {
+    let mut parser = tree_sitter::Parser::new();
+    parser
+      .set_language(super::language())
+      .expect("Error loading asena language");
+  }
 }
