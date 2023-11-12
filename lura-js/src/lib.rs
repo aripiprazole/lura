b893a971876e3d01030b514ce94add7d7f146279
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2023-11-12 19:57:16 -0300

    feat(lura-js): add base to generate js

--- lura-js/src/lib.rs
+++ lura-js/src/lib.rs
@@ -1,6 +1,8 @@
 use std::{path::Path, rc::Rc};
 
 use deno_core::{error::AnyError, FastString};
+use lura_hir::{source::expr::Expr, solver::Definition, HirDb};
+use resast::prelude as js;
 
 pub async fn run_js(file_path: &str) -> Result<(), AnyError> {
   let main_module = deno_core::resolve_path(file_path, Path::new("."))?;
@@ -16,4 +18,32 @@ pub async fn run_js(file_path: &str) -> Result<(), AnyError> {
   let result = js_runtime.mod_evaluate(mod_id);
   js_runtime.run_event_loop(false).await?;
   result.await?
+}
+
+pub fn definition_mangle_name(db: &dyn HirDb, definition: Definition) -> String {
+  let mut name = String::new();
+  for segment in definition.name(db).segments(db) {
+    name.push_str(&segment.contents(db));
+    name.push_str("_");
+  }
+  name
+}
+
+pub fn walk_on_expr<'a>(db: &dyn HirDb, expr: Expr) -> js::Expr<'a> {
+  match expr {
+    Expr::Empty => todo!(),
+    Expr::Error(_) => todo!(),
+    Expr::Path(reference) => {
+      let definition = reference.definition(db);
+      let mangled_name = definition_mangle_name(db, definition);
+      js::Expr::Ident(js::Ident::new(mangled_name))
+    },
+    Expr::Meta(_) => todo!(),
+    Expr::Literal(_) => todo!(),
+    Expr::Call(_) => todo!(),
+    Expr::Ann(_) => todo!(),
+    Expr::Abs(_) => todo!(),
+    Expr::Match(_) => todo!(),
+    Expr::Upgrade(_) => todo!(),
+  }
 }
