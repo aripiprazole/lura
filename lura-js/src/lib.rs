c1c7dee25d1660b385bb11a23ee7d117eeb4dfe9
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2023-11-13 19:55:59 -0300

    feat(lura-js): add some stuff

--- lura-js/src/lib.rs
+++ lura-js/src/lib.rs
@@ -2,7 +2,10 @@ use std::{path::Path, rc::Rc};
 
 use deno_core::{error::AnyError, FastString};
 use eyre::bail;
+use lura_hir::solver::Reference;
 use lura_hir::source::declaration::Declaration;
+use lura_hir::source::expr::Callee;
+use lura_hir::source::pattern::Pattern;
 use lura_hir::source::top_level::TopLevel;
 use lura_hir::{
   solver::Definition,
@@ -36,18 +39,39 @@ pub fn definition_mangle_name(db: &dyn HirDb, definition: Definition) -> String 
   name
 }
 
-pub fn walk_on_expr<'a>(db: &dyn HirDb, expr: Expr) -> js::Expr<'a> {
+pub fn compile_path(db: &dyn HirDb, path: Reference) -> js::Ident {
+  let definition = path.definition(db);
+  let mangled_name = definition_mangle_name(db, definition);
+  js::Ident::new(mangled_name)
+}
+
+pub fn compile_callee<'a>(db: &dyn HirDb, callee: Callee) -> js::Expr<'a> {
+  match callee {
+    Callee::Array => todo!(),
+    Callee::Tuple => todo!(),
+    Callee::Unit => todo!(),
+    Callee::Pure => todo!(),
+    Callee::Do => todo!(),
+    Callee::Reference(path) => js::Expr::Ident(compile_path(db, path)),
+    Callee::Expr(expr) => compile_expr(db, *expr),
+  }
+}
+
+pub fn compile_expr<'a>(db: &dyn HirDb, expr: Expr) -> js::Expr<'a> {
   match expr {
     Expr::Empty => todo!(),
     Expr::Error(_) => todo!(),
-    Expr::Path(reference) => {
-      let definition = reference.definition(db);
-      let mangled_name = definition_mangle_name(db, definition);
-      js::Expr::Ident(js::Ident::new(mangled_name))
-    }
+    Expr::Path(reference) => js::Expr::Ident(compile_path(db, reference)),
     Expr::Meta(_) => todo!(),
     Expr::Literal(_) => todo!(),
-    Expr::Call(_) => todo!(),
+    Expr::Call(call) => js::Expr::Call(js::CallExpr {
+      callee: compile_callee(db, call.callee).into(),
+      arguments: call
+        .arguments
+        .into_iter()
+        .map(|argument| compile_expr(db, argument))
+        .collect(),
+    }),
     Expr::Ann(_) => todo!(),
     Expr::Abs(_) => todo!(),
     Expr::Match(_) => todo!(),
@@ -55,15 +79,37 @@ pub fn walk_on_expr<'a>(db: &dyn HirDb, expr: Expr) -> js::Expr<'a> {
   }
 }
 
-pub fn transform_top_level(db: &dyn HirDb, top_level: TopLevel) -> eyre::Result<js::ProgramPart> {
+pub struct PatternMatching {
+  pub scrutinee: Vec<Expr>,
+  pub patterns: im::HashMap<Vec<Pattern>, Expr>,
+}
+
+pub fn compile_pattern_matching<'a>(db: &dyn HirDb, matching: PatternMatching) -> js::Expr<'a> {
+  todo!()
+}
+
+pub fn compile_top_level(db: &dyn HirDb, top_level: TopLevel) -> eyre::Result<js::ProgramPart> {
   match top_level {
     TopLevel::Error(_) => bail!("errors are not supported"),
     TopLevel::Using(_) => bail!("using are not supported"),
     TopLevel::Command(_) => bail!("commands are not supported"),
-    TopLevel::BindingGroup(group) => Ok(js::ProgramPart::decl(js::Decl::Func(js::Func {
+    TopLevel::BindingGroup(group) => Ok(js::ProgramPart::Decl(js::Decl::Func(js::Func {
       id: Some(js::Ident::new(group.name(db).to_string(db))),
       params: vec![],
-      body: js::FuncBody(vec![]),
+      body: js::FuncBody(vec![js::ProgramPart::Stmt(js::Stmt::Return(Some(
+        compile_pattern_matching(db, PatternMatching {
+          scrutinee: group
+            .clauses(db)
+            .into_iter()
+            .map(|clause| clause.arguments(db))
+            .collect(),
+          patterns: group
+            .clauses(db)
+            .into_iter()
+            .map(|clause| (clause.arguments(db), clause.value(db)))
+            .collect(),
+        }),
+      )))]),
       generator: false,
       is_async: false,
     }))),
@@ -82,7 +128,7 @@ pub fn dump_into_string<W: std::io::Write>(
 ) -> eyre::Result<()> {
   let mut writer = resw::Writer::new(w);
   for top_level in source.contents(db) {
-    let Ok(program_part) = transform_top_level(db, *top_level) else {
+    let Ok(program_part) = compile_top_level(db, *top_level) else {
       continue;
     };
 
